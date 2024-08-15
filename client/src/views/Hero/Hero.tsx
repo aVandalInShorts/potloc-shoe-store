d4604e818672cd3b82a08ec3130610f5c06a90b8
François Vandal <francois.vandal@gmail.com>
2024-08-15 13:36:42 -0400

    Hero and button and section

--- client/src/views/Hero/Hero.tsx
+++ client/src/views/Hero/Hero.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Logo from "@/svg/logo.svg";
 import Link from "next/link";
+import styles from "./Hero.module.css";
 
 export default function Hero(props: {
 	mainTitle: string;
@@ -10,20 +13,40 @@ export default function Hero(props: {
 		url: string;
 	}[];
 }) {
+	const [animStart, setAnimStart] = useState(false);
+
+	useEffect(() => {
+		// The animation will start after the component is mounted
+		// to prevent the animation from starting during loading
+		setAnimStart(true);
+		console.log("sasas");
+	}, []);
+
 	return (
-		<section>
-			<Logo />
-			<h1>{props.mainTitle}</h1>
-			<h2>{props.otherTitle}</h2>
-			{props.button?.length && (
-				<div>
-					{props.button.map((button) => (
-						<Link key={button.value} href={button.url}>
-							{button.value}
-						</Link>
-					))}
-				</div>
-			)}
+		<section
+			className={`section ${styles.hero}${animStart ? " anim-start" : ""}`}
+		>
+			<div className={`section-content-narrow ${styles["hero-content"]}`}>
+				<Logo className={styles.logo} />
+				<h1 className={styles["main-title"]}>{props.mainTitle}</h1>
+				<h2 className={styles["other-title"]}>{props.otherTitle}</h2>
+				{props.button?.length && (
+					<div className={styles.btns}>
+						{props.button.map((button, index) => (
+							<Link
+								className={`button button-${
+									index % 2 === 0 ? "green" : "orange"
+								}`}
+								key={`hero-button-${index}`}
+								href={button.url}
+								data-text={button.value}
+							>
+								{button.value}
+							</Link>
+						))}
+					</div>
+				)}
+			</div>
 		</section>
 	);
 }
